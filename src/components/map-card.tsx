da506d168a8d1c47aa7c8b082d410d0fd0f343bf
Nitai J. Perez <nitaiperez@gmail.com>
2024-06-06 00:32:27 +0300

    Update color to use style

--- src/components/map-card.tsx
+++ src/components/map-card.tsx
@@ -6,10 +6,10 @@ enum Type {
 }
 
 enum Color {
-    Black = "bg-black",
-    Red = "bg-[#dc4347]",
-    Blue = "bg-[#3c83b1]",
-    Blank = "bg-[#e6dfa7]",
+    Black = "#111111",
+    Red = "#dc4347",
+    Blue = "#3c83b1",
+    Blank = "#e6dfa7",
 }
 
 function shuffle(array: Type[]): Type[] {
@@ -52,29 +52,18 @@ function MapCard() {
     return (
         <div className="w-full h-screen py-12 flex items-center justify-center text-[#dc4347]">
             <div className="grid gap-5 w-full max-w-xl mx-auto p-5 items-center" style={{ height: 'calc(100vh - 24px)' }}>
-                <h1 className={clsx("text-center text-4xl bold", styleMap[Type.Random].replace("bg-", "text-"))}>{styleMap[Type.Random] === Color.Red ? "Red" : "Blue"} Starts</h1>
+                <h1 className="text-center text-4xl bold" style={{color: styleMap[Type.Random]}}>{styleMap[Type.Random] === Color.Red ? "Red" : "Blue"} Starts</h1>
                 <div className="bg-slate-900 p-5 rounded-xl border-2 border-slate-700 relative" style={{ aspectRatio: '1' }}>
-                    <div className={clsx(
-                        styleMap[Type.Random],
-                        "absolute h-3 w-1/3 top-5 left-1/2 transform -translate-x-1/2 -translate-y-[calc(50%-1px)] rounded-xl border-2 border-slate-700"
-                    )}></div>
-                    <div className={clsx(
-                        styleMap[Type.Random],
-                        "absolute h-3 w-1/3 bottom-5 left-1/2 transform -translate-x-1/2 translate-y-[calc(50%-1px)] rounded-xl border-2 border-slate-700"
-                    )}></div>
-                    <div className={clsx(
-                        styleMap[Type.Random],
-                        "absolute w-3 h-1/3 left-5 top-1/2 transform -translate-y-1/2 -translate-x-[calc(50%-1px)] rounded-xl border-2 border-slate-700"
-                    )}></div>
-                    <div className={clsx(
-                        styleMap[Type.Random],
-                        "absolute w-3 h-1/3 right-5 top-1/2 transform -translate-y-1/2 translate-x-[calc(50%-1px)] rounded-xl border-2 border-slate-700"
-                    )}></div>
+                    <div className="absolute h-3 w-1/3 top-5 left-1/2 transform -translate-x-1/2 -translate-y-[calc(50%-1px)] rounded-xl border-2 border-slate-700" style={{backgroundColor: styleMap[Type.Random]}}></div>
+                    <div className="absolute h-3 w-1/3 bottom-5 left-1/2 transform -translate-x-1/2 translate-y-[calc(50%-1px)] rounded-xl border-2 border-slate-700" style={{backgroundColor: styleMap[Type.Random]}} />
+                    <div className="absolute w-3 h-1/3 left-5 top-1/2 transform -translate-y-1/2 -translate-x-[calc(50%-1px)] rounded-xl border-2 border-slate-700" style={{backgroundColor: styleMap[Type.Random]}}/>
+                    <div className="absolute w-3 h-1/3 right-5 top-1/2 transform -translate-y-1/2 translate-x-[calc(50%-1px)] rounded-xl border-2 border-slate-700"  style={{backgroundColor: styleMap[Type.Random]}} />
                     <div
                         className="grid grid-cols-5 gap-2 bg-slate-900 p-5 rounded-xl border-2 border-slate-700 w-full">
                         {cells.map((cell, i) => (
-                            <div className={clsx("aspect-square rounded-xl w-full border border-slate-500", styleMap[cell])}
-                                 style={{ aspectRatio: '1 / 1' }} key={i}></div>
+                            <div className="aspect-square rounded-xl w-full border border-slate-500"
+                                 style={{backgroundColor: styleMap[cell], aspectRatio: '1 / 1'}}
+                                  key={i}></div>
                         ))}
                     </div>
                 </div>
